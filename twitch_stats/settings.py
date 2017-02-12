9b52570133cc88af70178fe9bdcc700aaff6972c
Ignas <Hoffs-@users.noreply.github.com>
2017-02-12 23:38:09 +0200

    Update settings.py
    pushed secret and id again....

--- twitch_stats/settings.py
+++ twitch_stats/settings.py
@@ -3,7 +3,7 @@
 """
 
 # Twitch App settings
-TWITCH_CLIENT_ID = "wzfatxi0lrgmnpvibgdqnokgtgkicv"
-TWITCH_CLIENT_SECRET = "eldblfgw9tj4wd9dk9enwxg39doai0"
+TWITCH_CLIENT_ID = ""
+TWITCH_CLIENT_SECRET = ""
 TWITCH_REDIRECT_URI = "http://localhost:4200/twitch/callback"
 TWITCH_VERSION_HEADERS = "application/vnd.twitchtv.v5+json"
