67c030f403ee14464ea48d09a90cbf2ac76f7edc
Ignas Maslinskas <masle.com@gmail.com>
2017-02-21 00:10:33 +0200

    heroku testing

--- twitch_stats/settings.py
+++ twitch_stats/settings.py
@@ -3,8 +3,10 @@
 """
 
 # Twitch App settings
-TWITCH_CLIENT_ID = ""
-TWITCH_CLIENT_SECRET = ""
+import os
+
+TWITCH_CLIENT_ID = os.environ.get('TWITCH_CLIENT_ID', '')
+TWITCH_CLIENT_SECRET = os.environ.get('TWITCH_CLIENT_SECRET', '')
 TWITCH_REDIRECT_URI = "https://hoff.pw/twitch/callback"
 TWITCH_VERSION_HEADERS = "application/vnd.twitchtv.v5+json"
 
